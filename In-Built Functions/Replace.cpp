6755bec116527380d728a5077683ae2fcd1dcb96
RaunakMishra1 <raunak37098@gmail.com>
2026-05-26 23:10:14 +0530

    Update Replace.cpp

--- In-Built Functions/Replace.cpp
+++ In-Built Functions/Replace.cpp
@@ -6,9 +6,9 @@ int main()
     getline(cin,str);
     /*
     replace (position,length,string)
-    Position(Index):From where to remove string or character
+    Position(Index):From where to remove string 
     Length: Count of character to remove from string
-    string/character:Add String/Character from that position only.
+    String:Add String from that position only.
     */
     str.replace(12,4,"C++");
     cout<<str;
